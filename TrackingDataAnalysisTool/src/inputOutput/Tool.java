3c9a1fc9f2c437698d74d8ce19d1521e34ec360b
Mannes <mannes@hs-ulm.de>
2018-04-23 17:19:03 +0200

    Constructor erstellt

--- TrackingDataAnalysisTool/src/inputOutput/Tool.java
+++ TrackingDataAnalysisTool/src/inputOutput/Tool.java
@@ -12,8 +12,17 @@ public class Tool {
 
 	private String name;
 
-	public Tool() {
-
+	public Tool(double coordinate_x, double coordinate_y, double coordinate_z, 
+				double rotation_x, double rotation_y, double rotation_z,
+				int timestamp, String name) {
+		this.coordinate_x = coordinate_x;
+		this.coordinate_y = coordinate_y;
+		this.coordinate_z = coordinate_z;
+		this.rotation_x = rotation_x;
+		this.rotation_y = rotation_y;
+		this.rotation_z = rotation_z;
+		this.timestamp =timestamp;
+		this.name = name;
 	}
 
 }
