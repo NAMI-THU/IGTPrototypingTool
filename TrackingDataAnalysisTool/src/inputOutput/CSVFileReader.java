488e87cfaed5a8b189864924ad42ac01e5bf53b8
Engeser <engeser@hs-ulm.de>
2018-04-23 17:33:49 +0200

    no message

--- TrackingDataAnalysisTool/src/inputOutput/CSVFileReader.java
+++ TrackingDataAnalysisTool/src/inputOutput/CSVFileReader.java
@@ -4,6 +4,93 @@ import java.io.*;
 
 public class CSVFileReader extends Interface {
 
+	
+	static int i = 0;
+
+	
+	
+	
+	
+	
+	
+	
+	
+	private static void init() {
+
+		String line = null;
+		
+		BufferedReader csv_file = null; 
+		try {
+			
+			
+			
+			csv_file=new BufferedReader(new InputStreamReader(new FileInputStream("U:\\Desktop\\logfile.csv")));
+
+			
+		} catch (Exception e) {
+		}
+
+		try {
+			
+			
+			for(int j = 0; j <= i ; j++){
+			 line = csv_file.readLine();}
+			
+			
+			
+			System.out.println(line);
+			i++;
+
+		} catch (IOException e) {
+			System.out.println("Read error " + e);
+
+		}
+
+	}
+
+	
+	
+	
+	
+	
+	
+	private void read(){
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+
+		init();
+		System.out.println("____________________________________");
+		init();
+		
+
+	}
 
 }
-
