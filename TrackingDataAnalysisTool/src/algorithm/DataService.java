2a154364eeb7db720e6b45b5ca36d1740a591a52
Berisha <aberisha@hs-ulm.de>
2018-04-23 16:40:56 +0200

    create method bodies loadNextData(),  getAverageMeasurement(), getJitter() and getAccuracy()

--- TrackingDataAnalysisTool/src/algorithm/DataService.java
+++ TrackingDataAnalysisTool/src/algorithm/DataService.java
@@ -1,5 +1,13 @@
 package algorithm;
 
 public class DataService {
-
+	
+	public void loadNextData() {}
+	
+	public void getAverageMeasurement() {}
+	
+	public void getJitter() {}
+	
+	public void getAccuracy() {}
+	
 }
