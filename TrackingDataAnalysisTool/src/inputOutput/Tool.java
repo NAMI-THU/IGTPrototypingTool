2694ac7db02cd95a1d8cbe008c2066b082b4884b
Bolek <bolek@hs-ulm.de>
2018-04-23 15:01:53 +0200

    Create Variables for Tool

--- TrackingDataAnalysisTool/src/inputOutput/Tool.java
+++ TrackingDataAnalysisTool/src/inputOutput/Tool.java
@@ -1,11 +1,19 @@
 package inputOutput;
 
 public class Tool {
-	
-	private double coordinate_x;
-	
-	public Tool(){
-		
+
+	private double coordinate_x, coordinate_y, coordinate_z;
+
+	private double rotation_x, rotation_y, rotation_z, rotation_c;
+
+	private boolean valid;
+
+	private int timestamp;
+
+	private String name;
+
+	public Tool() {
+
 	}
 
 }
