26bfeeb2a23e2c8e0ca6c838b23b192279cee4dd
Puschmann <puschmann@hs-ulm.de>
2018-04-30 14:20:39 +0200

    create class Point and methods

---
+++ TrackingDataAnalysisTool/src/algorithm/Point.java
@@ -0,0 +1,50 @@
+package algorithm;
+
+public class Point {
+	
+	private double x;
+	private double y;
+	private double z;
+	private double jitter;
+	
+	public Point(double x, double y, double z) {
+		this.x=x;
+		this.y=y;
+		this.z=z;
+	}
+	
+	public double getX() {
+		return x;
+	}
+	
+	public void setX(double x) {
+		this.x=x;
+	}
+	
+	public double getY() {
+		return y;
+	}
+	
+	public void setY(double y) {
+		this.y=y;
+	}
+
+	public double getZ() {
+		return z;
+	}
+
+
+	public void setZ(double z) {
+		this.z = z;
+	}
+	
+	public double getJitter() {
+		return jitter;
+	}
+	
+	public void setJitter(double jitter) {
+		this.jitter=jitter;
+	}
+	
+	
+}
