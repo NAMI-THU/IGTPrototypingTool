7ab57fda1c13217b5a8297e313ff6c00ad45e6bb
Puschmann <puschmann@hs-ulm.de>
2018-04-30 14:23:30 +0200

    create methods

--- TrackingDataAnalysisTool/src/algorithm/DataProcessor.java
+++ TrackingDataAnalysisTool/src/algorithm/DataProcessor.java
@@ -1,22 +1,53 @@
 package algorithm;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DataProcessor {
-	
-	
-public void getAccuracy () {
-	 
-} 
 
+	public ToolMeasure getAverageMeasurement(ToolMeasure tool) {
+		return tool;
+	}
 
+	public ToolMeasure getJitter(ToolMeasure tool) {
 
-public void getAverageMeasurement () {
-	 
-}
+		List<Measurement> measurements = tool.getMeasurement(); // Messungen von dem Tool
+		Measurement averageMeasurement = tool.getAverageMeasurement(); // durschnittliche Messung von dem Tool
 
+		for (int f = 0; f < averageMeasurement.getPoints().size(); f++) {
+			Point avgPoint = averageMeasurement.getPoints().get(f); // Punkte der durchschnittlichen Messung
+			double appendedDistance = 0;
+			for (int i = 0; i < measurements.size(); i++) {
+				Measurement measurement = measurements.get(i);
+				Point point = measurement.getPoints().get(f);
 
+				double distance = getDistance(point, avgPoint);
 
-public void getJitter () {
-	 
-}
+				appendedDistance += distance;
+				point.setJitter(distance);
 
+				// Durchschnitt bei letzer Messung berechnen
+
+				if (i == measurements.size() - 1) {
+					double avgDistance = appendedDistance / measurements.size();
+					avgPoint.setJitter(avgDistance);
+				}
+
+			}
+		}
+
+		return tool;
+	}
+
+	// Methode zum Berechnen der Distanz zweier Punkte
+
+	private double getDistance(Point firstPoint, Point secondPoint) {
+
+		double dX = Math.pow(firstPoint.getX() - secondPoint.getX(), 2);
+		double dY = Math.pow(firstPoint.getY() - secondPoint.getY(), 2);
+		double dZ = Math.pow(firstPoint.getZ() - secondPoint.getZ(), 2);
+
+		return Math.sqrt(dX + dY + dZ);
+
+	}
 }
