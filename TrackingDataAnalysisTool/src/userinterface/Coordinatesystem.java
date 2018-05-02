77220afe600360422643416a0f1fbc191cc225f9
Kiskac <kiskac@hs-ulm.de>
2018-05-02 12:44:09 +0200

    added HBox VBox Layout

--- TrackingDataAnalysisTool/src/userinterface/Coordinatesystem.java
+++ TrackingDataAnalysisTool/src/userinterface/Coordinatesystem.java
@@ -2,15 +2,28 @@ package userinterface;
 
 import java.util.List;
 
+import java.util.Arrays;
+
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.chart.XYChart.Series;
-import java.util.List;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.stage.Stage;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.ScatterChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.application.*;
 
 
-
-@SuppressWarnings("unused")	
-public class Coordinatesystem {
+//@SuppressWarnings("unused")	
+public class Coordinatesystem extends Application{
 
 	
 	/*public Coordinatesystem () {
@@ -32,14 +45,15 @@ public class Coordinatesystem {
 		}
           }*/
 
-@SupressWarnings({"unchecked", "rawtypes"})
+//@SupressWarnings({"unchecked", "rawtypes"})
 
-public static void drawAchsen(String choice, List<String>I,
-		XYChart.Series s, NumberAxis xAxis, NumberAxis yAxis) ){
+public static void drawAchsen(String choice, List<String>l,
+		XYChart.Series s, NumberAxis xAxis, NumberAxis yAxis) {
 			int x, y, z;
 			
 			
-xAxis.setLabel(arg0);
+			
+xAxis.setLabel("X-Achse");
 yAxis.setLabel("Y-Achse");
 		for (int i=0; i<l.size(); i=i+3) {
 			x = Integer.parseInt(l.get(i));
@@ -75,6 +89,73 @@ yAxis.setLabel("Y-Achse");
 				s.getData().add(new XYChart.Data(z, y));
 			}
 		}
+}
+
+		
+
+		public void xyz(){
+				
+				Button add, start;
+				//@SupressWarnings("unchecked")
+
+				
+				final NumberAxis xAxis = new NumberAxis(0, 200, 10);
+				final NumberAxis yAxis = new NumberAxis(0, 200, 10);
+				final NumberAxis zAxis = new NumberAxis(0, 200, 10);
+				
+				final ScatterChart <Number, Number> s1 = 
+						new ScatterChart<Number, Number>(xAxis, yAxis);
+				final ScatterChart <Number, Number> s2 = 
+						new ScatterChart<Number, Number>(xAxis, zAxis);
+				final ScatterChart <Number, Number> s3 = 
+						new ScatterChart<Number, Number>(yAxis, zAxis);
+				
+			
+		XYChart.Series series1 = new XYChart.Series();
+		
+		s1.setPrefSize(100, 50);
+		
+		final VBox vbox = new VBox();
+		final HBox hbox = new HBox();
+		
+		RadioButton radioB1 = new RadioButton("xyz");
+		start = new Button("Start");
+		
+		try {
+			series1.getData().clear();
+				String choice = "xyz";
+				
+				List <String> line = null;
+				Coordinatesystem.drawAchsen(choice, line, series1, xAxis, yAxis);
+		}
+		catch(InterruptedException e){
+			e.printStackTrace();
+		
+		}
+				hbox.setSpacing(10);
+		hbox.getChildren().addAll(start, s1, s2, s3);
+		
+	vbox.getChildren().addAll(hbox);
+	hbox.setPadding(new Insets(10, 10, 10, 50));
 		}
 
 
+
+}
+
+
+@Override
+public void start(Stage primaryStage) throws Exception {
+	// TODO Auto-generated method stub
+
+
+	
+}
+
+public static void main(String[]args){
+	
+	launch(args);}}
+
+
+
+	
