819ddf40e36661216286169ac2168eb49baef768
Engeser <engeser@hs-ulm.de>
2018-05-02 14:42:26 +0200

    String array cornverted into a Double array Put the variables via constructor into tool-object Creat a new Tool

--- TrackingDataAnalysisTool/src/inputOutput/CSVFileReader.java
+++ TrackingDataAnalysisTool/src/inputOutput/CSVFileReader.java
@@ -23,32 +23,34 @@ public class CSVFileReader extends Interface {
 		}
 
 		try {
+
+			
 			
 			for (int j = 0; j <= i; j++) {
 				line = csv_file.readLine();
 				data = line.split(";");
 
 			}
-			
-			
-			int number_of_tools = (data.length)/9;
-			
-			//System.out.println(number_of_tools);
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
 
+			double[] data_new = new double[data.length];
+
+			if(i==1){
+			for (int a = 0; a < data.length; a++) {
+
+				data_new[a] = Double.parseDouble(data[a]);
+			}
+
+			int number_of_tools = (data.length) / 9;
+
+			// System.out.println(number_of_tools);
+
+			//for (int i = 1; i <= number_of_tools; i++) {
+
+				Tool tool = new Tool(data_new[0],data_new[1],data_new[2],data_new[3],data_new[4],data_new[5],data_new[6],data_new[7],data_new[8],"Test");
+
+			//}
+
+			}
 			for (int n = 0; n < data.length; n++) {
 				System.out.println(data[n]);
 			}
@@ -62,7 +64,7 @@ public class CSVFileReader extends Interface {
 
 	public static void main(String[] args) {
 
-		for (int i = 0; i <= 0; i++) {
+		for (int i = 0; i <= 1; i++) {
 
 			read();
 			System.out.println("____________________________________");
