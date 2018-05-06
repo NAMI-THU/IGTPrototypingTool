5b04c2841b8158ea573707843367c074c8d8dc57
Anna Hauser <hauser@mail.hs-ulm.de>
2018-05-06 20:18:28 +0200

    Create JUnit Test Case for the methode getAverageMeasurement().

--- TrackingDataAnalysisTool/src/algorithm/AlgorithmTest2.java
+++ TrackingDataAnalysisTool/src/algorithm/AlgorithmTest2.java
@@ -19,10 +19,52 @@ public class AlgorithmTest2 {
 
 	@Test
 	public void getAverageMeasurementIsCorrect() {
-	
+
+		setUpData();
+
+		ToolMeasure result = processor.getAverageMeasurement(testTool);
+
+		Point expectedPoint = new Point(2,2,2);
+		Point avgPoint = result.getAverageMeasurement().getPoints().get(0);
+		assertTrue(avgPoint.getX() == expectedPoint.getX());
+		assertTrue(avgPoint.getY() == expectedPoint.getY());
+		assertTrue(avgPoint.getZ() == expectedPoint.getZ());
 
 	}
 
-	
+	private void setUpData() {
+
+		processor = new DataProcessor();
+		testTool = new ToolMeasure("TestTool");
+
+		List<Point> points1 = new ArrayList<>();
+		List<Point> points2 = new ArrayList<>();
+		List<Point> points3 = new ArrayList<>();
+
+		Measurement measurement1 = new Measurement();
+		Measurement measurement2 = new Measurement();
+		Measurement measurement3 = new Measurement();
+
+		Point p1 = new Point(1, 1, 1);
+		Point p2 = new Point(2, 2, 2);
+		Point p3 = new Point(3, 3, 3);
+
+		points1.add(p1);
+		points2.add(p2);
+		points3.add(p3);
+
+		measurement1.setPoints(points1);
+		measurement2.setPoints(points2);
+		measurement3.setPoints(points3);
+
+		testTool.addMeasurement(measurement1);
+		testTool.addMeasurement(measurement2);
+		testTool.addMeasurement(measurement3);
+
+	}
 
 }
+
+
+
+
