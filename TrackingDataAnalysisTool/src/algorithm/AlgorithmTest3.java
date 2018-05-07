4a39d3a1d89ac2c5b68d73c18202c44d1b069fa4
Mahmood <mahmood@mail.hs-ulm.de>
2018-05-07 14:29:37 +0200

    Created and implemented JUnitTest for method getAccuracy ()

--- TrackingDataAnalysisTool/src/algorithm/AlgorithmTest3.java
+++ TrackingDataAnalysisTool/src/algorithm/AlgorithmTest3.java
@@ -14,15 +14,36 @@ import algorithm.ToolMeasure;
 
 public class AlgorithmTest3 {
 
-	private DataProcessor processor;
-	private ToolMeasure testTool;
 
-	@Test
-	public void getAccuracyIsCorrect() {
-	
+		private DataProcessor processor;
+		List<Point> firstPoints;
+		List<Point> secondPoints;
+
+		@Test
+		public void getAccuracyCalculationIsCorrect() {
+
+			setUpData();
+			double expectedDistance = 1.7320508075688772;
+
+			double result = processor.getAccuracy(expectedDistance, firstPoints, secondPoints);
+
+			// keine Abweichung erwartet
+			assertTrue(result == 0);
+		}
+
+		private void setUpData() {
+			processor = new DataProcessor();
+
+			firstPoints = new ArrayList<>();
+			secondPoints = new ArrayList<>();
+
+			Point p1 = new Point(1, 1, 1);
+			Point p4 = new Point(2, 2, 2);
+
+			firstPoints.add(p1);
+			secondPoints.add(p4);
+		}
 
 	}
-
-	
 
 }
