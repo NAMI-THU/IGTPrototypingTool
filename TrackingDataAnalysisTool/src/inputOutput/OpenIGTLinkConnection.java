b0fe6b0c578169454b18a5d43f25d8863c460e7c
Alfred Franz <franz@hs-ulm.de>
2018-07-14 00:27:40 +0200

    made OpenIGTLinkConnection object oriented and deriving from TrackingDataSource. Also adapted simple test app.

--- TrackingDataAnalysisTool/src/inputOutput/OpenIGTLinkConnection.java
+++ TrackingDataAnalysisTool/src/inputOutput/OpenIGTLinkConnection.java
@@ -6,10 +6,21 @@ import java.util.ArrayList;
  * can be transfered directly without saving it in a CSV-file in advance
  */
 
+
+
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 import com.neuronrobotics.sdk.addons.kinematics.math.TransformNR;
+import com.neuronrobotics.sdk.common.Log;
 import com.sun.j3d.utils.geometry.compression.CompressedGeometryData.Header;
 
-public class OpenIGTLinkConnection {
+public class OpenIGTLinkConnection extends TrackingDataSource{
 
 	private double coordinate_x;
 	private double coordinate_y;
@@ -21,26 +32,59 @@ public class OpenIGTLinkConnection {
 	private String name;
 	private double timestamp;
 	private double valid = 1;
-	private ArrayList<Tool> toollist = new ArrayList<Tool>();
-	private int counter = 0;
 	private int counter1 = 1;
 	private String helpname;
 	private static boolean testapp = false;
 
-	// public static void update() {
-	//
-	// Networkconnection.Connection();
-	//
-	// }
-
 	
+	private String ipAddress = "127.0.0.1";
+	private int port = 18944;
+	private Networkconnection myOpenIGTLinkConnection;
+
+	@Override
+	public ArrayList<Tool> update() {
+		if (myOpenIGTLinkConnection == null) {
+			myOpenIGTLinkConnection = new Networkconnection();
+			myOpenIGTLinkConnection.Connect(ipAddress, port);
+			toollist = new ArrayList<Tool>();
+		}
+		List<Networkconnection.ToolData> rawToolList = myOpenIGTLinkConnection.getToolDataList();
+		synchronized (rawToolList) {
+			for(Networkconnection.ToolData t : rawToolList) this.setValues(t.name, t.t);
+		}
+		
+		return toollist;
+	}
+	
+	public String getIpAddress() {
+		return ipAddress;
+	}
+
+
+	public void setIpAddress(String ipAddress) {
+		this.ipAddress = ipAddress;
+	}
+
+
+	public int getPort() {
+		return port;
+	}
+
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+	
+	
+
+
 	/**
 	 * this method assigns the values of each tool to an object of a toollist
 	 * @param n is the name of the tool
 	 * @param t is a matrix which tells us the coordinates and the rotation of each tool
 	 */
 	public void setValues(String n, TransformNR t) {
-
+		
 		timestamp = org.medcare.igtl.util.Header.getTimeStamp();
 
 		name = n;
@@ -53,55 +97,29 @@ public class OpenIGTLinkConnection {
 		rotation_y = t.getRotation().getRotationMatrix2QuaturnionY();
 		rotation_z = t.getRotation().getRotationMatrix2QuaturnionZ();
 
-		if (counter1 == 1) {
-
-			helpname = name;
-
+		for (Tool cur_tool : toollist)
+		{
+			if(cur_tool.getName().equals(n)) 
+				{
+				cur_tool.setData(timestamp, valid, coordinate_x, coordinate_y,
+					coordinate_z, rotation_x, rotation_y, rotation_z, rotation_r,
+					name);
+				return;	
+				}
 		}
+		
 
-		Tool tool = new Tool();
-
-		tool.setData(timestamp, valid, coordinate_x, coordinate_y,
+		Tool newTool = new Tool();
+		newTool.setData(timestamp, valid, coordinate_x, coordinate_y,
 				coordinate_z, rotation_x, rotation_y, rotation_z, rotation_r,
 				name);
-
-		setArraylist(tool);
-
-		counter1++;
-
+		this.toollist.add(newTool);
+		
 	}
 	
+	public void closeConnection()
+	{
+		myOpenIGTLinkConnection.setExit(false);
+	}
 	
-	/**
-	 * this method creates an ArrayList for the data of the tools containing as many objects as there are tools available
-	 * @param tool contains the values of one tool
-	 */
-
-	public void setArraylist(Tool tool) {
-
-		if (helpname.equals(name) && counter1 != 1) {
-
-			// set toollist in DataManager from group2
-			algorithm.DataManager data = new algorithm.DataManager();
-			data.setList(toollist);
-
-			// call method to get output in console
-			if (testapp == true) {
-				testInputOutput.Networkconnection_test_app
-						.setTestlist(toollist);
-			}
-
-			toollist.clear();
-			counter1 = 1;
-		}
-
-		toollist.add(tool);
-
-	}
-
-	//use for Networkconnection_test_app
-	public static void setTestappValue() {
-		testapp = true;
-	}
-
 }
