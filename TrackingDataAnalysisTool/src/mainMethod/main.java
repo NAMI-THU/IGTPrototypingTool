3d394721fa943beced961017e2db99b9bd252f30
Alfred Franz <franz@hs-ulm.de>
2018-07-16 14:11:56 +0200

    launch app from main method now...

--- TrackingDataAnalysisTool/src/mainMethod/main.java
+++ TrackingDataAnalysisTool/src/mainMethod/main.java
@@ -1,31 +1,18 @@
 package mainMethod;
 
-import javax.swing.JFrame;
-
-import algorithm.*;
-import inputOutput.*;
 import userinterface.*;
 
 public class main {
 
 	public static void main(String[] args) {
-		
-		/*
-		Gui myGui = new Gui();
-		myGui.setSize(1000, 1000);
-		myGui.setTitle("TrackingDataAnalysisTool");
-		myGui.setLocation(600,30);
-		myGui.setVisible(true);
-		myGui.validate();
-		myGui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		*/
-		
-		
-        Diagramm app = new Diagramm();
-        //app.main(null);
-        
-       
-        
+
+		new Thread() {
+            @Override
+            public void run() {
+                javafx.application.Application.launch(Diagramm.class);
+            }
+        }.start();
+                  
 	}
 
 }
