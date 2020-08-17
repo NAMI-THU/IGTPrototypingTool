f327afea5393bcf558d29f43ca3079e22cb5c1dc
vseidel <vseidel@mail.hs-ulm.de>
2020-08-17 18:44:23 +0200

    Enable display of multiple tools in coordinate system, read from OpenIGTLinkConnection or CSV file

--- src/main/java/userinterface/Diagramm.java
+++ src/main/java/userinterface/Diagramm.java
@@ -10,6 +10,8 @@ import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.chart.NumberAxis;
@@ -27,8 +29,14 @@ import javafx.util.Duration;
 import java.io.File;
 import java.io.IOException;
 import java.text.DecimalFormat;
+import java.util.HashMap;
 import java.util.List;
 
+/* TODO create separate methods for different functionality
+ * make layout dynamic & better to use
+ * clean up code
+ * parameterize raw types
+ */
 public class Diagramm extends Application {
 
     /*
@@ -57,32 +65,57 @@ public class Diagramm extends Application {
     DataService da;
     MeasurementView myMeasurementView;
     Timeline timeline;
-    // series3.setName("YZ-Diagramm");
-    /*
-     * Create new series for the coordinate system and set name.
-     */
-    XYChart.Series series1 = new XYChart.Series();
-    // series1.setName("XY-Diagramm");
-    XYChart.Series series2 = new XYChart.Series();
-    // series2.setName("XZ-Diagramm");
-    XYChart.Series series3 = new XYChart.Series();
+    HashMap<String,XYChart.Series<Double,Double>[]> toolSeriesMap;
+    
     Label pos = new Label();
     Label rot = new Label();
-
-    @SuppressWarnings("unchecked")
 
     /*
      * Create a method which contains the stage. Stage is the main window and
      * all elements of the window are entered there
      */
-
     @Override
     public void start(Stage stage) throws InterruptedException {
 
-
         stage.setTitle("Tracking Data Analysis Tool");
+        /* create a new scene */
+        Scene scene = new Scene(new Group());
 
-        /* ############# create charts #############  */
+        // handle button events
+        choose.setOnAction(loadCSVFile());
+        network.setOnAction(connect());
+        start.setOnAction(visualizeTracking());
+        measurement.setOnAction(openMeasurementView());
+
+        /* create layouts */
+        VBox vbox = new VBox();
+        VBox v2box = new VBox();
+        VBox v3box = new VBox();
+        HBox hbox = new HBox();
+
+        pos.setText("Position: [-]");
+        rot.setText("Orientation: [-]");
+
+        /* Add buttons and charts to the layout */
+        vbox.setSpacing(10);
+        vbox.getChildren().addAll(choose, network, start, measurement);
+        v2box.getChildren().addAll(s1, s2);
+        v3box.getChildren().addAll(s3, pos, rot);
+        hbox.getChildren().addAll(v2box, v3box, vbox);
+
+        ((Group) scene.getRoot()).getChildren().add(hbox);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    
+    /*
+     * This class initializes the application. It is called immediately after Diagramm class
+     *  is loaded and constructed. Buttons and Labels are created.
+     */
+    @Override
+    public void init() {
+    	/* ############# create charts #############  */
         /* create variables x, y and z from type string to store each axis */
         String x = "X-axis";
         String y = "Y-axis";
@@ -100,29 +133,32 @@ public class Diagramm extends Application {
         s1.setTitle("XY-plane");
         s2.setTitle("XZ-plane");
         s3.setTitle("YZ-plane");
-
+        
+        toolSeriesMap = new HashMap<String, XYChart.Series<Double, Double>[]>();
+        
         /*
-         * set size for each scatter-chart and add the series on the
-         * scatter-charts, which contain the axes
+         * set size for each scatter-chart which contain the axes
          */
-        s1.setPrefSize(400, 300);
-        s1.getData().addAll(series1);
-
-        s2.setPrefSize(400, 300);
-        s2.getData().addAll(series2);
-
-        s3.setPrefSize(400, 300);
-        s3.getData().addAll(series3);
-
-        /* create a new scene */
-        Scene scene = new Scene(new Group());
-
-
+	    s1.setPrefSize(400, 300);
+	    s2.setPrefSize(400, 300);
+	    s3.setPrefSize(400, 300);
+        
         /* #############  create buttons  ############# */
         choose = new Button("Load Data From MITK CSV File");
         choose.setMinWidth(280);
-
-        choose.setOnAction((event) -> { // handle button event
+        network = new Button("Connect via OpenIGTLink (localhost)");
+        network.setMinWidth(280);
+        start = new Button("Start Tracking Data Visualization");
+        start.setMinWidth(280);
+        measurement = new Button("Open Measurement View");
+        measurement.setMinWidth(280);
+    }
+    
+    /*
+     * TODO abbrechen soll moeglich sein
+     */
+    private EventHandler<ActionEvent> loadCSVFile(){
+    	return event -> {
             CSVFileReader newSource = null;
             while (newSource == null) {
                 fp = new FileChooser();
@@ -143,33 +179,33 @@ public class Diagramm extends Application {
                     }
                 }
             }
-        });
-
-        network = new Button("Connect via OpenIGTLink (localhost)");
-        network.setMinWidth(280);
-
-        // handle button event
-        network.setOnAction((event) -> {
+    	};
+    }
+    
+    /*
+     * Connect via OpenIGTLink.
+     */
+    private EventHandler<ActionEvent> connect(){
+    	return event -> {
             OpenIGTLinkConnection newSource = new OpenIGTLinkConnection();
             source = newSource;
             if (myMeasurementView != null)
                 myMeasurementView.setTrackingDeviceSource(source);
-        });
-
-        /* create new button "start" with the name "Start" */
-        start = new Button("Start Tracking Data Visualization");
-        start.setMinWidth(280);
-
-        /*
-         * add action on the button "start" if the button is clicked, there will
-         * be shown the values x, y and z on the axes of the scatter-charts
-         */
-        start.setOnAction((event) -> {
-
+        };
+    }
+    
+    /*
+     * add action on the button "start" if the button is clicked, there will
+     * be shown the values x, y and z on the axes of the scatter-charts
+     */
+    private EventHandler<ActionEvent> visualizeTracking(){
+    	return event -> {
             if (timeline == null && source != null) {
-                series1.getData().clear();
-                series2.getData().clear();
-                series3.getData().clear();
+            	toolSeriesMap.forEach((tool,seriesarray) -> {
+            		for(XYChart.Series<Double, Double> series : seriesarray) {
+            			series.getData().clear();
+            		}
+            	});
 
                 // create an object from the class "DataService" in package
                 // algorithm
@@ -184,86 +220,84 @@ public class Diagramm extends Application {
                 timeline.play();
                 updateDiagrams();
             }
-
-        });
-
-        /* create new button "start" with the name "Start" */
-        measurement = new Button("Open Measurement View");
-        measurement.setMinWidth(280);
-
-        /*
-         * add action on the button "start" if the button is clicked, there will
-         * be shown the values x, y and z on the axes of the scatter-charts
-         */
-        measurement.setOnAction((event) -> {
+        };
+    }
+    
+    private EventHandler<ActionEvent> openMeasurementView(){
+    	return event -> {
             myMeasurementView = new MeasurementView(source);
             myMeasurementView.setSize(1000, 1000);
             myMeasurementView.setTitle("TrackingDataAnalysisTool");
             myMeasurementView.setLocation(600, 30);
             myMeasurementView.setVisible(true);
             myMeasurementView.validate();
-        });
-
-        /* create layouts */
-        VBox vbox = new VBox();
-        VBox v2box = new VBox();
-        VBox v3box = new VBox();
-        HBox hbox = new HBox();
-
-
-        pos.setText("Position: [-]");
-        rot.setText("Orientation: [-]");
-
-        /* Add buttons and charts to the layout */
-        vbox.setSpacing(10);
-        vbox.getChildren().addAll(choose, network, start, measurement);
-        v2box.getChildren().addAll(s1, s2);
-        v3box.getChildren().addAll(s3, pos, rot);
-        hbox.getChildren().addAll(v2box, v3box, vbox);
-
-        ((Group) scene.getRoot()).getChildren().add(hbox);
-        stage.setScene(scene);
-        stage.show();
-
+        };
     }
-
+    
     public void updateDiagrams() {
         // all Tools with all measurements
         source.update();
         List<ToolMeasure> tools = da.loadNextData(1);
         if (tools.isEmpty()) return;
-        ToolMeasure tool = tools.get(tools.size() - 1);
+        for(ToolMeasure tool : tools) {
+	
+	        // all measurements from one tool
+	        List<Measurement> li = tool.getMeasurement();
+	        
+	        if(!toolSeriesMap.containsKey(tool.getName())) {
+	        	createSeriesForTool(tool.getName());
+	        }
+	        
+	        // use correct series
+	        XYChart.Series<Double, Double>[] toolSeries = toolSeriesMap.get(tool.getName());
+	        for(XYChart.Series<Double, Double> s : toolSeries) {
+	        	s.getData().clear();
+	        }
+	
+	        for (int i = 1; i < 5; i++) { //use the last 5 measurements, otherwise blending will be a problem during motion
+	
+	            if (li.size() - i < 0) {
+	                break;
+	            }
+	
+	            double x = li.get(li.size() - i).getPoint().getX();
+	            double y = li.get(li.size() - i).getPoint().getY();
+	            double z = li.get(li.size() - i).getPoint().getZ();
 
-        // all measurements from one tool
-        List<Measurement> li = tool.getMeasurement();
-
-        series1.getData().clear();
-        series2.getData().clear();
-        series3.getData().clear();
-
-        for (int i = 1; i < 5; i++) { //use the last 5 measurements, otherwise blending will be a problem during motion
-
-            if (li.size() - i < 0) {
-                break;
-            }
-
-            double x = li.get(li.size() - i).getPoint().getX();
-            double y = li.get(li.size() - i).getPoint().getY();
-            double z = li.get(li.size() - i).getPoint().getZ();
-
-            if (i == 1) {
-                DecimalFormat df = new DecimalFormat("0.00");
-                double qX = li.get(li.size() - i).getRotation().getX();
-                double qY = li.get(li.size() - i).getRotation().getY();
-                double qZ = li.get(li.size() - i).getRotation().getZ();
-                double qR = li.get(li.size() - i).getRotation().getW();
-                pos.setText("Position: [" + df.format(x) + ";" + df.format(y) + ";" + df.format(z) + "]");
-                rot.setText("Orientation: [" + df.format(qX) + ";" + df.format(qY) + ";" + df.format(qZ) + ";" + df.format(qR) + "]");
-            }
-
-            series1.getData().add(new XYChart.Data(x, y));
-            series2.getData().add(new XYChart.Data(x, z));
-            series3.getData().add(new XYChart.Data(z, y));
+	            // maybe adjust to display this for every tool
+	            if (i == 1) {
+	                DecimalFormat df = new DecimalFormat("0.00");
+	                double qX = li.get(li.size() - i).getRotation().getX();
+	                double qY = li.get(li.size() - i).getRotation().getY();
+	                double qZ = li.get(li.size() - i).getRotation().getZ();
+	                double qR = li.get(li.size() - i).getRotation().getW();
+	                pos.setText("Position: [" + df.format(x) + ";" + df.format(y) + ";" + df.format(z) + "]");
+	                rot.setText("Orientation: [" + df.format(qX) + ";" + df.format(qY) + ";" + df.format(qZ) + ";" + df.format(qR) + "]");
+	            }
+	            
+	            toolSeries[0].getData().add(new XYChart.Data<Double, Double>(x,y));
+	            toolSeries[1].getData().add(new XYChart.Data<Double, Double>(x,z));
+	            toolSeries[2].getData().add(new XYChart.Data<Double, Double>(z,y));
+	        }
         }
+    }
+    
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+	private void createSeriesForTool(String toolname) {
+    	
+    	XYChart.Series[] seriesArray = new XYChart.Series[3];
+    	
+    	// Series needs to have a dataset so name and symbol are set correctly
+    	for(int i=0; i<3; i++) {
+    		seriesArray[i] = new XYChart.Series();
+    		seriesArray[i].getData().add(new XYChart.Data(0,0));
+    		seriesArray[i].setName(toolname);
+    	}
+    	
+    	toolSeriesMap.put(toolname, seriesArray);
+    	
+    	s1.getData().addAll(seriesArray[0]);
+    	s2.getData().addAll(seriesArray[1]);
+    	s3.getData().addAll(seriesArray[2]);
     }
 }
