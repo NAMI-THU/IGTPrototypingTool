f438ee1c9187db252bd8edac3dbb4af956b1f376
vseidel <vseidel@mail.hs-ulm.de>
2020-12-01 13:43:13 +0100

    add formatmanager class and css stylesheet

---
+++ src/main/java/util/FormatManager.java
@@ -0,0 +1,25 @@
+package util;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
+/**
+ * This class includes custom formats to use in this project.
+ *
+ */
+public class FormatManager {
+	
+	public static String toString(Double input) {
+		DecimalFormat df = new DecimalFormat("#0.00",
+			new DecimalFormatSymbols(Locale.US));
+		return df.format(input);
+	}
+	
+	public static String toString(Object input) {
+		DecimalFormat df = new DecimalFormat("#0.00",
+				new DecimalFormatSymbols(Locale.US));
+			return df.format(input);
+	}
+
+}
