f68ffddcb59cb0cfc79eb5c4855daa068d5ad640
vseidel <vseidel@mail.hs-ulm.de>
2020-12-14 15:18:15 +0100

    Start Application from separate class, so Javafx runtime components are found correctly by Gradle

---
+++ src/main/java/mainMethod/App.java
@@ -0,0 +1,37 @@
+package mainMethod;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import util.CustomLogger;
+
+public class App extends Application {
+
+	private final static Logger LOGGER = Logger.getLogger(java.util.logging.Logger.GLOBAL_LOGGER_NAME);
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        // setup logging
+        try {
+            CustomLogger.setup();
+        } catch (IOException e) {
+            LOGGER.warning("Log file was not set up");
+            e.printStackTrace();
+        }
+
+        String path = "/view/MainView.fxml";
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource(path));
+        Parent root = loader.load();
+        primaryStage.setTitle("Tracking Data Analysis Tool");
+        Scene scene = new Scene(root, 1050, 700);
+        scene.getStylesheets().add("css/customstyle.css");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
