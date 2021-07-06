66af7682eee571746e1d280a9e2996752dba62ea
DESKTOP-KJ9S9J2\Vero <veronika.seidel95@web.de>
2021-07-06 17:23:56 +0200

    Video View: add option to load image data over OpenIGTLink connection

--- src/main/java/controller/VideoController.java
+++ src/main/java/controller/VideoController.java
@@ -1,6 +1,5 @@
 package controller;
 
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -9,11 +8,9 @@ import algorithm.ImageDataManager;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
-import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
@@ -46,11 +43,19 @@ public class VideoController implements Controller {
         case "Webcam":
             this.dataManager.openConnection(0);
             break;
+        case "OpenIGTLink":
+        	this.dataManager.openConnection(1);
+            break;
         case "Video file":
-            this.loadFile();
+        	File file = this.loadFile();
+            if(file != null) {
+                this.dataManager.getDataProcessor().setFilePath(file.getAbsolutePath());
+                this.dataManager.openConnection(2);
+            }
             break;
         case "External device":
-            break;
+        	break;
+
         }
     }
 
@@ -73,24 +78,16 @@ public class VideoController implements Controller {
     }
 
     private void update() {
-// old way to load images with awt
-//        BufferedImage bufImg = dataManager.readBufImg();
-//        Image img = SwingFXUtils.toFXImage(bufImg, null);
-//        iv.setImage(img);
-
         iv.setImage(dataManager.readImg());
     }
 
-    private void loadFile() {
+    private File loadFile() {
         FileChooser fc = new FileChooser();
         FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter("Video files","*.avi","*.mp4", "*.mkv", "*.mov", "*.3GP", "*.mpg");
         fc.setSelectedExtensionFilter(filter);
         File file = fc.showOpenDialog(new Stage());
 
-        if(file != null) {
-            this.dataManager.getDataProcessor().setFilePath(file.getAbsolutePath());
-            this.dataManager.openConnection(2);
-        }
+        return file;
     }
 
 }
