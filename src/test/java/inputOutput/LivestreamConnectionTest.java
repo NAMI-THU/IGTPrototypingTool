116adab8599fb2e964431a9bafc40e14bdd5405a
Veronika Seidel <vseidel@mail.hs-ulm.de>
2021-09-13 11:40:18 +0200

    Rename test classes and exclude OpenCV tests on master, so build is not broken on TravisCI

--- src/test/java/inputOutput/LivestreamConnectionTest.java
+++ src/test/java/inputOutput/LivestreamConnectionTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Test;
  * @author sahin
  *
  */
-class LiveStreamConnectionTest {
+class LivestreamConnectionTest {
 
     /**
      * This method tests instantiating the class LivestreamSource.
