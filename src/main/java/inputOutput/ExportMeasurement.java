02bd5b70fb2f907702bb59a703b5f810fee9bc40
Timo <43245438+TheRisenPhoenix@users.noreply.github.com>
2022-03-17 17:17:08 +0100

    Expose attributes in ExportMeasurement
    Expose the attributes in ExportMeasurement so that gson writes them to the json file

--- src/main/java/inputOutput/ExportMeasurement.java
+++ src/main/java/inputOutput/ExportMeasurement.java
@@ -1,9 +1,22 @@
 package inputOutput;
 
+import com.google.gson.annotations.Expose;
+
 public class ExportMeasurement {
+    @Expose
     public String toolName;
-    public double x_shifted, y_shifted, z_shifted;
-    public double x_raw, y_raw, z_raw;
+    @Expose
+    public double x_shifted;
+    @Expose
+    public double y_shifted;
+    @Expose
+    public double z_shifted;
+    @Expose
+    public double x_raw;
+    @Expose
+    public double y_raw;
+    @Expose
+    public double z_raw;
 
     public ExportMeasurement(String toolName, double x_raw, double y_raw, double z_raw, double x_shifted, double y_shifted, double z_shifted) {
         this.toolName = toolName;
