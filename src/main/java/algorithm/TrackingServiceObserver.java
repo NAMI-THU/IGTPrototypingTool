66c5fa2866f56de5903c41c59dd5bf6eb0114d97
Timo <43245438+TheRisenPhoenix@users.noreply.github.com>
2022-05-02 12:24:36 +0200

    Added documentation for TrackingService

--- src/main/java/algorithm/TrackingServiceObserver.java
+++ src/main/java/algorithm/TrackingServiceObserver.java
@@ -1,9 +1,14 @@
 package algorithm;
 
-import algorithm.DataService;
-import inputOutput.AbstractTrackingDataSource;
-import javafx.animation.Timeline;
-
+/**
+ * Interface for defining a callback-method for being triggered upon a change in references in TrackingService.
+ */
 public interface TrackingServiceObserver {
+    /**
+     * This method is triggered once a reference to one of the attributes of the TrackingService changed
+     * @param sourceChanged true, if the TrackingSource changed
+     * @param dataServiceChanged true, if the DataService changed
+     * @param timelineChanged true, if the timeline changed.
+     */
     void onTrackingSourceChanged(boolean sourceChanged, boolean dataServiceChanged, boolean timelineChanged);
 }
