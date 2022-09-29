38c54053fb0e5431137fd7dd3ee8ec596f019e6c
rsteinborn <ra.steinborn@gmail.com>
2022-09-29 15:20:23 +0200

    added local X Y Z Axis for Rotation

--- src/main/java/shapes/TrackingSphere.java
+++ src/main/java/shapes/TrackingSphere.java
@@ -19,11 +19,11 @@ public class TrackingSphere extends Sphere {
     Field variables
      */
     private final TrackingService trackingService = TrackingService.getInstance();
-    private Sphere[] trackingSpheres;
+//    private Sphere[] trackingSpheres;
 
-    final Rotate rx = new Rotate(0, Rotate.X_AXIS);
-    final Rotate ry = new Rotate(0, Rotate.Y_AXIS);
-    final Rotate rz = new Rotate(0, Rotate.Z_AXIS);
+    public Rotate rx = new Rotate();
+    public Rotate ry = new Rotate();
+    public Rotate rz = new Rotate();
 
 
     /*
@@ -31,6 +31,9 @@ public class TrackingSphere extends Sphere {
      */
     public TrackingSphere(double radius, int divisions, Color color) {
         super(radius, divisions);
+        rx.setAxis(Rotate.X_AXIS);
+        ry.setAxis(Rotate.Y_AXIS);
+        rz.setAxis(Rotate.Z_AXIS);
         setMaterial(new PhongMaterial(color));
         getTransforms().addAll(rz, ry, rx);
     }
@@ -38,20 +41,20 @@ public class TrackingSphere extends Sphere {
     /*
     Methods
      */
-    public Sphere[] loadTrackingSpheres() {
-        List<ToolMeasure> tools = trackingService.getDataService().loadNextData(1);
-
-
-
-        if (tools != null) {
-            trackingSpheres = new Sphere[tools.size()];
-            int x = 0;
-
-            for (ToolMeasure tool : tools) {
-                trackingSpheres[x] = new Sphere();
-                x++;
-            }
-        }
-        return trackingSpheres;
-    }
+//    public Sphere[] loadTrackingSpheres() {
+//        List<ToolMeasure> tools = trackingService.getDataService().loadNextData(1);
+//
+//
+//
+//        if (tools != null) {
+//            trackingSpheres = new Sphere[tools.size()];
+//            int x = 0;
+//
+//            for (ToolMeasure tool : tools) {
+//                trackingSpheres[x] = new Sphere();
+//                x++;
+//            }
+//        }
+//        return trackingSpheres;
+//    }
 }
