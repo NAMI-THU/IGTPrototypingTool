2a5733c82a2e6cbded226cf4bcf44654581053f0
Alfred Franz <alfred.franz@thu.de>
2024-10-29 16:48:31 +0100

    Added example for grabbing a video image

--- src/main/java/controller/ExampleController.java
+++ src/main/java/controller/ExampleController.java
@@ -1,9 +1,13 @@
 package controller;
+import algorithm.ImageDataManager;
+import algorithm.ImageDataProcessor;
 import algorithm.Measurement;
 import algorithm.TrackingService;
+import inputOutput.VideoSource;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 
 import java.net.URL;
 import java.util.List;
@@ -11,11 +15,15 @@ import java.util.ResourceBundle;
 
 public class ExampleController implements Controller {
 
-    
+    @FXML
+    public Label trackingDataLabel;
     @FXML
     public Button exampleButton;
     @FXML
-    public Label trackingDataLabel;
+    public ImageView imageView;
+
+    private boolean videoConnected = false;
+    private ImageDataManager imageDataManager = new ImageDataManager();
     
 
     @Override
@@ -29,6 +37,7 @@ public class ExampleController implements Controller {
     }
 
     public void on_exampleButton(){
+        //Example for getting tracking data
         String s = "";
         TrackingService t = TrackingService.getInstance();
         if (t.getTrackingDataSource()!=null) {
@@ -41,7 +50,22 @@ public class ExampleController implements Controller {
         }
         trackingDataLabel.setText(s);
         System.out.println(s);
-    }
 
+
+    }
+    public void  on_exampleVideoButton(){
+        //Example for grabbing a video stream from device id 0
+        if(!videoConnected) {
+            videoConnected = imageDataManager.openConnection(VideoSource.LIVESTREAM, 0);
+        }
+        if(videoConnected){
+            imageView.setImage(imageDataManager.readImg());
+            System.out.println("Updated video");
+        }
+        else{
+            System.out.println("Could not connect to video source id 0");
+        }
+
+    }
 
 }
